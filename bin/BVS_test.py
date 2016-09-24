3411c89596466f6746cf10ceeaa6ba3c5488b732
Sebastian Cepeda F <sebastian.cepeda.fuentealba@outlook.com>
2016-09-24 11:36:42 -0300

    Se leen parámetros desde archivo .json

--- bin/BVS_test.py
+++ bin/BVS_test.py
@@ -21,16 +21,9 @@ import datetime
 from sklearn import preprocessing
 from sklearn.metrics import roc_auc_score
 import utils
+import json
 ###############################
-print('Start! ')
-it_counter = 0
-ImageShape     = (584,565,3)
-PatternShape   = (584,565)
-winSize            = (15, 15)
-n_features       = 3*winSize[0]*winSize[1]
-alpha               = 1
-###############################
-def load_data(n_image = 21):
+def load_data(ImageShape, PatternShape, winSize, n_image = 21):
     print('Reading data. ')
     features 	   = Image.open('../DRIVE/training/images/'+str(n_image)+'_training.tif','r')    
     features     = numpy.fromstring(features.tobytes(), dtype='uint8', count=-1, sep='')
@@ -46,11 +39,12 @@ def load_data(n_image = 21):
     train_set = preprocessing.scale(train_set)
     return train_set, train_set_t
 
-def build_custom_mlp(input_var=None, depth=2, width=800, drop_input=.2, drop_hidden=.5):
+def build_custom_mlp(n_features, input_var=None, depth=2, width=800, drop_input=.2, drop_hidden=.5):
     network = lasagne.layers.InputLayer(shape=(None, n_features), input_var=input_var)
     if drop_input:
         network = lasagne.layers.dropout(network, p=drop_input)
     # Hidden layers and dropout:
+    #nonlin = lasagne.nonlinearities.very_leaky_rectify
     nonlin = lasagne.nonlinearities.leaky_rectify
     for _ in range(depth):
         network = lasagne.layers.DenseLayer(network, width, nonlinearity=nonlin)
@@ -62,33 +56,59 @@ def build_custom_mlp(input_var=None, depth=2, width=800, drop_input=.2, drop_hid
     return network
 
 # ############################## Main program ################################
-def main(model='custom_mlp', num_epochs=500):
+def main():
+    print('* Start! ')
+    print('* Loading config.json')
+    with open('config.json') as json_data:
+        config = json.load(json_data)
+        depth = int(config["layers"])
+        width = int(config["neurons_by_layer"])
+        drop_in = float(config["dropout_input"])
+        drop_hid = float(config["dropout_hidden"])
+        num_epochs = int(config["num_epochs"])    
+        winSide = int(config["window_side"])    
+        ImageShape = config["image_shape"]
+        ImageShape = (int(ImageShape[0]),int(ImageShape[1]),int(ImageShape[2]))
+        alpha = float(config["alpha"])
+    # Other global variables
+    PatternShape   	= (ImageShape[0],ImageShape[1])
+    winSize        	    = (winSide,winSide)
+    n_features     	    = ImageShape[2]*(winSide**2)
+    print("* Building model and compiling functions...")
     input_var = T.dmatrix('inputs')
     target_var = T.ivector('targets')
-    print("Building model and compiling functions...")
-    if model.startswith('custom_mlp:'):
-        depth, width, drop_in, drop_hid = model.split(':', 1)[1].split(',')
-        network = build_custom_mlp(input_var, int(depth), int(width), float(drop_in), float(drop_hid))
-    else:
-        print("Unrecognized model type %r." % model)
-        return
+    network = build_custom_mlp(n_features, input_var, depth, width, drop_in, drop_hid)
     prediction = lasagne.layers.get_output(network)
     t2 = theano.tensor.extra_ops.to_one_hot(target_var, 2, dtype='int32')
     error = lasagne.objectives.categorical_crossentropy(prediction, t2)
     loss = error.mean()/n_features
     params = lasagne.layers.get_all_params(network, trainable=True)
     train_fn = theano.function([input_var, target_var], [loss])
-    test_prediction = lasagne.layers.get_output(network, deterministic=True)
-    test_loss = ((test_prediction[:, 1]-target_var)*(test_prediction[:, 1]-target_var)).mean()
-    test_loss = test_loss.mean()
     output_model = theano.function([input_var], prediction)
-    # grads
+    # grads compilation
     grads = []
     for w in params:
         grad = T.grad(loss,  w)
         grad_fn = theano.function([input_var, target_var], grad)
         grads = grads + [grad_fn]
     
+    def params_giver():
+        ws = []
+        for w in params:
+            params_fn = theano.function([],  w)
+            w_out = params_fn()
+            ws = numpy.append(ws,  w_out)
+        return ws
+    
+    def grad_giver(x,  t,  p_data):
+        gs = []
+        for grad_fn in grads:
+            gs = numpy.append(gs,  grad_fn(x,  t))
+        return gs
+    '''
+    Method that receives the new set of weights 
+    and inserts them in the net. 
+    '''
     def params_updater(all_w):
         idx_init = 0
         for w in params:
@@ -108,7 +128,7 @@ def main(model='custom_mlp', num_epochs=500):
     print('Show test images... ')
     for i in numpy.arange(21, 41):
         print('Test image: {}'.format(i))
-        X_train, y_train = load_data(i)
+        X_train, y_train = load_data(ImageShape, PatternShape, winSize, i)
         y_preds  = output_model(X_train)
         output_image = utils.reconstruct_image(y_preds,w=winSize, PatternShape=PatternShape, alpha=alpha)
         aux_image = output_image
@@ -116,18 +136,4 @@ def main(model='custom_mlp', num_epochs=500):
         cv2.imwrite('debug/y_preds-'+str(i)+'.png',img)
 
 if __name__ == '__main__':
-    if ('--help' in sys.argv) or ('-h' in sys.argv):
-        print("Trains a neural network on RedHat using Lasagne.")
-        print("Usage: %s [MODEL [EPOCHS]]" % sys.argv[0])
-        print()
-        print("MODEL: 'custom_mlp:DEPTH,WIDTH,DROP_IN,DROP_HID' for an MLP")
-        print("       with DEPTH hidden layers of WIDTH units, DROP_IN")
-        print("       input dropout and DROP_HID hidden dropout,")
-        print("EPOCHS: number of training epochs to perform (default: 500)")
-    else:
-        kwargs = {}
-        if len(sys.argv) > 1:
-            kwargs['model'] = sys.argv[1]
-        if len(sys.argv) > 2:
-            kwargs['num_epochs'] = int(sys.argv[2])
-        main(**kwargs)
+    main()
